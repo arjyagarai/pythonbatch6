2c34499d388d6139930e2f68f123e017a31501ea
IF-BLR-LT-148\Admin <arjya.garai@indusface.com>
2020-05-12 11:21:16 +0530

    Testing

--- testgit.py
+++ testgit.py
@@ -2,7 +2,7 @@ name = input("Please enter your preferred Porgraming lang:")
 print("Before converting in Caps:"+name)
 capname = name.upper()
 
-print("After converting in Caps:"+capname)
+print("After converting in Capital letter:"+capname)
 
 for letter in capname:
     print(letter)
